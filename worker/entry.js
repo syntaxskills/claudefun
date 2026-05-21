601ba37919b884e7214cab748c5d385c8041d375
tankztz <tankztz@gmail.com>
2026-05-21 18:38:22 +0800

    Add Cloudflare deploy entrypoint

---
+++ worker/entry.js
@@ -0,0 +1,9 @@
+import openNextWorker, {
+  BucketCachePurge,
+  DOQueueHandler,
+  DOShardedTagCache,
+} from "../.open-next/worker.js";
+
+export { BucketCachePurge, DOQueueHandler, DOShardedTagCache };
+
+export default openNextWorker;
